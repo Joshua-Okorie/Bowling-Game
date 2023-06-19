c5fb653f30ce171f134c3c20ea729aa7f0d86da8
Arktic-Panda <joshuaokorie98@outlook.com>
2023-06-19 16:58:16 +0100

    Immplementing Ball Movement

--- Assets/Scripts/BallControler.cs
+++ Assets/Scripts/BallControler.cs
@@ -4,15 +4,54 @@ using UnityEngine;
 
 public class BallControler : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Rigidbody ballRigidBody;
+    int forceMultiplier;
+    private float elapsedTime = 0f;
+    public float increaseInterval = 1f; 
+    
+
     void Start()
     {
-        
+        ballRigidBody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
-    {
-        
+    {   
+       if(Input.GetKey(KeyCode.Space))
+        {
+            ForceAdd();
+        }
+          
+        if(Input.GetKeyUp(KeyCode.Space))
+        {
+            ImpulseForce(forceMultiplier);
+
+        } 
     }
+
+    public void ImpulseForce(int forceMultiplier)
+    {
+        Vector3 ballDrive = new Vector3(-5.0f -forceMultiplier, 0f, 0f);
+        ballRigidBody.AddForce(ballDrive,ForceMode.Impulse);
+    }
+
+    public void ForceAdd()
+    {
+        {
+            elapsedTime += Time.deltaTime;
+
+            if (elapsedTime >= increaseInterval)
+            {
+                forceMultiplier++;
+                elapsedTime = 0f;
+                Debug.Log("FORCE" + forceMultiplier);
+                
+            }
+            
+        }
+
+    }
+
+   
 }
